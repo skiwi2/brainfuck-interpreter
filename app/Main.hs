d6610501924d3f76fd641c6095c1f266e0d14f7a
skiwi <frank_skiwi@hotmail.com>
2018-03-25 22:41:05 +0200

    Added initial version, nested loops do not work yet.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
 import Lib
+import System.Environment
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case args of
+        [program]   -> interpret program
+        _           -> putStrLn "Usage: brainfuck-interpreter-exe <program>"
