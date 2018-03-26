9f3b514cc8f618fbb40d37d691d66a2546d50edf
skiwi <frank_skiwi@hotmail.com>
2018-03-26 22:18:10 +0200

    Implemented support for nested loops up to some degree, bugs are still to be expected.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Control.Monad
 import Lib
 import System.Environment
 
@@ -8,5 +7,9 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [program]   -> void . interpret $ program
+        [program]   -> do
+            result <- interpret program
+            case result of
+                Left err    -> putStrLn ("Error: " ++ err)
+                Right _     -> return ()
         _           -> putStrLn "Usage: brainfuck-interpreter-exe <program>"
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,7 @@ module Lib
 import Data.Char
 import Data.List
 
-interpret :: [Char] -> IO [Int]
+interpret :: [Char] -> IO (Either String [Int])
 interpret program = do
     step [] program [0] 0
 
@@ -13,36 +13,59 @@ interpret program = do
 -- TODO: error handling
 -- TODO: introduce types
 -- TODO: fix matching brackets bug
-step :: [Char] -> [Char] -> [Int] -> Int -> IO [Int]
-step _ [] memory _ = return memory
-step previousProgram nextProgram memory pointer = do
-    let instruction = head nextProgram
+step :: [Char] -> [Char] -> [Int] -> Int -> IO (Either String [Int])
+step _ [] memory _ = return . Right $ memory
+step previousProgram currentProgram memory pointer = do
+    let Just (instruction, nextProgram) = uncons currentProgram
     let (previousMemory, currentMemory) = splitAt pointer memory
     let currentMemoryCell = head currentMemory
     let nextMemory = tail currentMemory
     case instruction of
         '>' -> if pointer == length memory - 1
-            then step (previousProgram ++ [instruction]) (tail nextProgram) (memory ++ [0]) (length memory)
-            else step (previousProgram ++ [instruction]) (tail nextProgram) memory (pointer + 1)
+            then step (previousProgram ++ [instruction]) nextProgram (memory ++ [0]) (length memory)
+            else step (previousProgram ++ [instruction]) nextProgram memory (pointer + 1)
         '<' -> if pointer == 0
-            then step (previousProgram ++ [instruction]) (tail nextProgram) ([0] ++ memory) 0
-            else step (previousProgram ++ [instruction]) (tail nextProgram) memory (pointer - 1)
-        '+' -> step (previousProgram ++ [instruction]) (tail nextProgram) (previousMemory ++ [currentMemoryCell + 1] ++ nextMemory) pointer
-        '-' -> step (previousProgram ++ [instruction]) (tail nextProgram) (previousMemory ++ [currentMemoryCell - 1] ++ nextMemory) pointer
+            then step (previousProgram ++ [instruction]) nextProgram ([0] ++ memory) 0
+            else step (previousProgram ++ [instruction]) nextProgram memory (pointer - 1)
+        '+' -> step (previousProgram ++ [instruction]) nextProgram (previousMemory ++ [currentMemoryCell + 1] ++ nextMemory) pointer
+        '-' -> step (previousProgram ++ [instruction]) nextProgram (previousMemory ++ [currentMemoryCell - 1] ++ nextMemory) pointer
         '.' -> do
             putChar . chr $ currentMemoryCell
-            step (previousProgram ++ [instruction]) (tail nextProgram) memory pointer
+            step (previousProgram ++ [instruction]) nextProgram memory pointer
         ',' -> do
             newCurrentChar <- getChar
             let newCurrent = ord newCurrentChar
-            step (previousProgram ++ [instruction]) (tail nextProgram) (previousMemory ++ [newCurrent] ++ nextMemory) pointer
+            step (previousProgram ++ [instruction]) nextProgram (previousMemory ++ [newCurrent] ++ nextMemory) pointer
         '[' -> case currentMemoryCell of
-            0   -> do
-                let Just advancement = elemIndex ']' (tail nextProgram)
-                step (previousProgram ++ [instruction] ++ (take advancement nextProgram)) (drop (advancement + 1) nextProgram) memory pointer
-            _   -> step (previousProgram ++ [instruction]) (tail nextProgram) memory pointer
+            0   -> case findMatchingLoopClose nextProgram 0 0 of
+                Left err            -> return . Left $ err
+                Right advancement   -> step (previousProgram ++ [instruction] ++ (take advancement currentProgram)) (drop (advancement + 1) currentProgram) memory pointer
+            _   -> step (previousProgram ++ [instruction]) nextProgram memory pointer
         ']' -> case currentMemoryCell of
-            0   -> step (previousProgram ++ [instruction]) (tail nextProgram) memory pointer
-            _   -> do
-                let newStart = last $ elemIndices '[' previousProgram
-                step (take newStart previousProgram) ((drop newStart previousProgram) ++ [instruction] ++ nextProgram) memory pointer
+            0   -> step (previousProgram ++ [instruction]) nextProgram memory pointer
+            _   -> case findMatchingLoopOpen previousProgram 0 0 of
+                Left err        -> return . Left $ err
+                Right decrease  -> do
+                    let newStart = (length previousProgram) - decrease
+                    step (take newStart previousProgram) ((drop newStart previousProgram) ++ currentProgram) memory pointer
+
+findMatchingLoopClose :: [Char] -> Int -> Int -> Either String Int
+findMatchingLoopClose [] _ _ = Left "findMatchingLoopClose: No matching ] found"
+findMatchingLoopClose (instruction:remainingProgram) index nestingCounter = case instruction of
+    '[' -> findMatchingLoopClose remainingProgram (index + 1) (nestingCounter + 1)
+    ']' -> if nestingCounter == 0
+        then Right index
+        else findMatchingLoopClose remainingProgram (index + 1) (nestingCounter - 1)
+    _   -> findMatchingLoopClose remainingProgram (index + 1) nestingCounter
+
+findMatchingLoopOpen :: [Char] -> Int -> Int -> Either String Int
+findMatchingLoopOpen = findMatchingLoopOpen' . reverse
+
+findMatchingLoopOpen' :: [Char] -> Int -> Int -> Either String Int
+findMatchingLoopOpen' [] _ _ = Left "findMatchingLoopOpen: No matching [ found"
+findMatchingLoopOpen' (instruction:remainingProgram) index nestingCounter = case instruction of
+    ']' -> findMatchingLoopOpen' remainingProgram (index + 1) (nestingCounter + 1)
+    '[' -> if nestingCounter == 0
+        then Right index
+        else findMatchingLoopOpen' remainingProgram (index + 1) (nestingCounter - 1)
+    _   -> findMatchingLoopOpen' remainingProgram (index + 1) nestingCounter
