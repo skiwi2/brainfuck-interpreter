7c6f842a2c07d88d44667d4cdcd20f467356fd6a
skiwi <frank_skiwi@hotmail.com>
2018-03-27 20:27:02 +0200

    Added option to read from file.

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,15 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [program]   -> do
-            result <- interpret program
-            case result of
-                Left err    -> putStrLn ("Error: " ++ err)
-                Right _     -> return ()
-        _           -> putStrLn "Usage: brainfuck-interpreter-exe <program>"
+        [program]       -> interpret' program
+        ["-f", file]    -> do
+            contents <- readFile file
+            interpret' contents
+        _               -> putStrLn "Usage: brainfuck-interpreter-exe <program> or brainfuck-interpret-exe -f <file>"
+
+interpret' :: String -> IO ()
+interpret' program = do
+    result <- interpret program
+    case result of
+        Left err    -> putStrLn ("Error: " ++ err)
+        Right _     -> return ()
